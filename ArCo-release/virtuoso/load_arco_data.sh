0d303de91b293d602f84fb910c16b31106388932
anuzzolese <andrea.nuzzolese@gmail.com>
2018-03-09 15:23:21 +0100

    Updated docker.

--- ArCo-release/virtuoso/load_arco_data.sh
+++ ArCo-release/virtuoso/load_arco_data.sh
@@ -1,49 +1,3 @@
-#!/bin/bash
-SETTINGS_DIR=/settings
-mkdir -p $SETTINGS_DIR
-
-cd /data
-
-mkdir -p dumps
-
-if [ ! -f ./virtuoso.ini ];
-then
-  mv /virtuoso.ini . 2>/dev/null
-fi
-
-chmod +x /clean-logs.sh
-mv /clean-logs.sh . 2>/dev/null
-
-#original_port=`crudini --get virtuoso.ini HTTPServer ServerPort`
-# NOTE: prevents virtuoso to expose on port 8890 before we actually run
-#		the server
-#crudini --set virtuoso.ini HTTPServer ServerPort 27015
-
-if [ ! -f "$SETTINGS_DIR/.config_set" ];
-then
-  echo "Converting environment variables to ini file"
-  printenv | grep -P "^VIRT_" | while read setting
-  do
-    section=`echo "$setting" | grep -o -P "^VIRT_[^_]+" | sed 's/^.\{5\}//g'`
-    key=`echo "$setting" | sed -E 's/^VIRT_[^_]+_(.*)=.*$/\1/g'`
-    value=`echo "$setting" | grep -o -P "=.*$" | sed 's/^=//g'`
-    echo "Registering $section[$key] to be $value"
-    crudini --set virtuoso.ini $section $key "$value"
-  done
-  echo "`date +%Y-%m%-dT%H:%M:%S%:z`" >  $SETTINGS_DIR/.config_set
-  echo "Finished converting environment variables to ini file"
-fi
-
-if [ ! -f ".dba_pwd_set" ];
-then
-  touch /sql-query.sql
-  if [ "$DBA_PASSWORD" ]; then echo "user_set_password('dba', '$DBA_PASSWORD');" >> /sql-query.sql ; fi
-  if [ "$SPARQL_UPDATE" = "true" ]; then echo "GRANT SPARQL_UPDATE to \"SPARQL\";" >> /sql-query.sql ; fi
-  virtuoso-t +wait && isql-v -U dba -P dba < /dump_nquads_procedure.sql && isql-v -U dba -P dba < /sql-query.sql
-  kill "$(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')"
-  echo "`date +%Y-%m-%dT%H:%M:%S%:z`" >  .dba_pwd_set
-fi
-
 if [ ! -f ".data_loaded" -a -d "toLoad" ] ;
 then
     echo "Start data loading from toLoad folder"
@@ -59,8 +13,8 @@ then
     echo "exec('checkpoint');" >> /load_data.sql
     echo "WAIT_FOR_CHILDREN; " >> /load_data.sql
     echo "$(cat /load_data.sql)"
-    virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_data.sql
-    kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
+    isql-v -U dba -P "$pwd" < /load_data.sql
+    #kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
 fi
 
 if [ ! -f ".arco_ontologies_loaded" -a -d "/usr/local/virtuoso-opensource/share/virtuoso/vad/ontologies/" ] ;
@@ -72,8 +26,8 @@ then
     echo "exec('checkpoint');" >> /load_arco_ontologies.sql
     echo "WAIT_FOR_CHILDREN; " >> /load_arco_ontologies.sql
     echo "$(cat /load_arco_ontologies.sql)"
-    virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_arco_ontologies.sql
-    kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
+    isql-v -U dba -P "$pwd" < /load_arco_ontologies.sql
+    #kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .arco_ontologies_loaded
 fi
 
@@ -86,8 +40,8 @@ then
     echo "exec('checkpoint');" >> /load_dbunico.sql
     echo "WAIT_FOR_CHILDREN; " >> /load_dbunico.sql
     echo "$(cat /load_dbunico.sql)"
-    virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_dbunico.sql
-    kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
+    isql-v -U dba -P "$pwd" < /load_dbunico.sql
+    #kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .dbunico_loaded
 fi
 
@@ -100,12 +54,7 @@ then
     echo "exec('checkpoint');" >> /load_arco_data.sql
     echo "WAIT_FOR_CHILDREN; " >> /load_arco_data.sql
     echo "$(cat /load_arco_data.sql)"
-    virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_arco_data.sql
-    kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
+    isql-v -U dba -P "$pwd" < /load_arco_data.sql
+    #kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .arco_data_loaded
 fi
-
-
-#crudini --set virtuoso.ini HTTPServer ServerPort ${VIRT_HTTPServer_ServerPort:-$original_port}
-
-exec virtuoso-t +wait +foreground
