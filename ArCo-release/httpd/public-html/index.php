a1dc55f6ed925f2c01e55e99b3ac9bccb9c78c93
anuzzolese <andrea.nuzzolese@gmail.com>
2019-07-26 12:23:09 +0200

    Update.

--- ArCo-release/httpd/public-html/index.php
+++ ArCo-release/httpd/public-html/index.php
@@ -2,9 +2,9 @@
   
 if(isset($_GET["lang"])){
 	$lang = $_GET["lang"];
-	if($lang == "en") include index-en.html;
-	else include index-it.html;
+	if($lang == "en") include "index-en.html";
+	else include "index-it.html";
 }
-else include index-it.html;
+else include "index-it.html";
 
 ?>
