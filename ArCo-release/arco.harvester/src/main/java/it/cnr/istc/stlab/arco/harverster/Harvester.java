c3ff567669617ac261bddd8164ce548cdd0504f4
Luigi Asprino <luigi.asprino@gmail.com>
2020-07-18 11:25:38 +0200

    harverster list update

--- ArCo-release/arco.harvester/src/main/java/it/cnr/istc/stlab/arco/harverster/Harvester.java
+++ ArCo-release/arco.harvester/src/main/java/it/cnr/istc/stlab/arco/harverster/Harvester.java
@@ -132,7 +132,7 @@ public class Harvester {
 					logger.error("Retry! " + i);
 				}
 			}
-			
+
 			if (c.longValue() >= this.items) {
 				break;
 			}
@@ -273,40 +273,11 @@ public class Harvester {
 
 	public static void main(String[] args) throws ParserConfigurationException, XPathExpressionException, IOException,
 			SAXException, TransformerException {
-		Harvester h = new Harvester("http://catalogo.beniculturali.it/oaitarget/OAIHandler?",
-				"/Users/lgu/Desktop/testRecords");
+		Harvester h = new Harvester(args[0], args[1]);
 		List<String> keycodes = new ArrayList<>();
-		keycodes.add("ICCD10015760");
-		keycodes.add("ICCD10045923");
-		keycodes.add("ICCD10055673");
-		keycodes.add("ICCD10082711");
-		keycodes.add("ICCD10115591");
-		keycodes.add("ICCD10239336");
-		keycodes.add("ICCD10266725");
-		keycodes.add("ICCD10524764");
-		keycodes.add("ICCD10533913");
-		keycodes.add("ICCD10561093");
-		keycodes.add("ICCD11177581");
-		keycodes.add("ICCD11306544");
-		keycodes.add("ICCD11306860");
-		keycodes.add("ICCD11306934");
-		keycodes.add("ICCD11979011");
-		keycodes.add("ICCD12270243");
-		keycodes.add("ICCD2100596");
-		keycodes.add("ICCD2237068");
-		keycodes.add("ICCD8353344");
-		keycodes.add("ICCD10042835");
-		keycodes.add("ICCD10095890");
-		keycodes.add("ICCD10113684");
-		keycodes.add("ICCD10322197");
-		keycodes.add("ICCD10522154");
-		keycodes.add("ICCD10616036");
-		keycodes.add("ICCD11251491");
-		keycodes.add("ICCD11251795");
-		keycodes.add("ICCD11324026");
-		keycodes.add("ICCD11434960");
-		keycodes.add("ICCD11574733");
-		keycodes.add("ICCD11689075");
+		for (int i = 2; i < args.length; i++) {
+			keycodes.add(args[i]);
+		}
 		h.getRecordsFromList(keycodes);
 	}
 
