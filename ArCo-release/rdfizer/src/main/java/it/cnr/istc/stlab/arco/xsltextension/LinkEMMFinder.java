6796256c2a3dc92eacee6424555394c1efdd6346
Luigi Asprino <luigi.asprino@gmail.com>
2020-09-04 09:50:52 +0200

    Link EMM escape added

--- ArCo-release/rdfizer/src/main/java/it/cnr/istc/stlab/arco/xsltextension/LinkEMMFinder.java
+++ ArCo-release/rdfizer/src/main/java/it/cnr/istc/stlab/arco/xsltextension/LinkEMMFinder.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.google.common.net.PercentEscaper;
+
 import it.cnr.istc.stlab.arco.preprocessing.PreprocessedData;
 import net.sf.saxon.s9api.ExtensionFunction;
 import net.sf.saxon.s9api.ItemType;
@@ -19,11 +21,12 @@ import net.sf.saxon.s9api.XdmValue;
 public class LinkEMMFinder implements ExtensionFunction {
 
 	private static LinkEMMFinder instance;
+	private PercentEscaper pe;
 	private Map<String, String> ftan2linkEMM;
 	private static final Logger logger = LogManager.getLogger(LinkEMMFinder.class);
 
 	private LinkEMMFinder() {
-
+		pe = new PercentEscaper("/:._,;<>!$\"'#[]*+@(){}=\\", false);
 		PreprocessedData pd = PreprocessedData.getInstance(true);
 		this.ftan2linkEMM = pd.getFtan2URL();
 
@@ -42,7 +45,9 @@ public class LinkEMMFinder implements ExtensionFunction {
 		logger.trace("Argument " + arg);
 		String url = ftan2linkEMM.get(arg);
 		logger.trace("Link emm " + url);
-		
+		url = pe.escape(url);
+		logger.trace("Escaped link " + url);
+
 		if (url == null || url.length() == 0)
 			return XdmEmptySequence.getInstance();
 		else
