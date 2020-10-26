93cd439bc536103ea7c50bdf7d644756b553ba81
Luigi Asprino <luigi.asprino@gmail.com>
2020-10-26 09:12:39 +0100

    DB auto-update implemented

--- ArCo-release/rdfizer/src/main/java/it/cnr/istc/stlab/arco/preprocessing/PreprocessedData.java
+++ ArCo-release/rdfizer/src/main/java/it/cnr/istc/stlab/arco/preprocessing/PreprocessedData.java
@@ -1,7 +1,9 @@
 package it.cnr.istc.stlab.arco.preprocessing;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
@@ -14,13 +16,15 @@ import org.mapdb.Serializer;
 
 public class PreprocessedData {
 
-	public static String dbFileName = "db", dbURL = "http://arco.istc.cnr.it/preprocessing/db";
+	public static String dbFileName = "db", dbURL = "http://arco.istc.cnr.it/preprocessing/db",
+			generatedURLString = "http://arco.istc.cnr.it/preprocessing/generated";
 
 	private DB db;
 	private static PreprocessedData instance;
 	private Map<String, String> ftan2URL, catalogueRecordIdentifier2URI;
 	private Map<String, List<String>> uniqueIdentifier2URIs;
 	private static final Logger logger = Logger.getLogger(PreprocessedData.class);
+	public static final String GENERATED = "GENERATED";
 
 	@SuppressWarnings("unchecked")
 	private PreprocessedData(boolean download) {
@@ -29,7 +33,6 @@ public class PreprocessedData {
 			try {
 				logger.info("Downloading preprocessed data from " + dbURL);
 				FileUtils.copyURLToFile(new URL(dbURL), new File(dbFileName));
-
 				logger.info(dbURL + " downloaded!");
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -38,14 +41,51 @@ public class PreprocessedData {
 			logger.info("Preprocessed data already exists! ");
 		}
 		this.db = DBMaker.fileDB(dbFileName).make();
-
 		this.ftan2URL = db.hashMap("ftan2URL").keySerializer(Serializer.STRING).valueSerializer(Serializer.JAVA)
 				.createOrOpen();
 		this.catalogueRecordIdentifier2URI = db.hashMap("catalogueRecordIdentifier2URI")
 				.keySerializer(Serializer.STRING).valueSerializer(Serializer.JAVA).createOrOpen();
-
 		this.uniqueIdentifier2URIs = db.hashMap("uniqueIdentifier2URIs").keySerializer(Serializer.STRING)
 				.valueSerializer(Serializer.JAVA).createOrOpen();
+
+		try {
+			org.mapdb.Atomic.Long generated = db.atomicLong(GENERATED).createOrOpen();
+			Long remoteGenerated = readRemoteGenerated();
+			logger.info("The timestamp of the remote db is " + remoteGenerated);
+			logger.info("The timestamp of the local db is " + generated);
+			if (remoteGenerated > generated.longValue()) {
+				logger.info("Updated DB");
+				this.db.commit();
+				this.db.close();
+				new File(dbFileName).delete();
+				logger.info("Downloading preprocessed data from " + dbURL);
+				FileUtils.copyURLToFile(new URL(dbURL), new File(dbFileName));
+				logger.info(dbURL + " downloaded!");
+				this.db = DBMaker.fileDB(dbFileName).make();
+				this.ftan2URL = db.hashMap("ftan2URL").keySerializer(Serializer.STRING).valueSerializer(Serializer.JAVA)
+						.createOrOpen();
+				this.catalogueRecordIdentifier2URI = db.hashMap("catalogueRecordIdentifier2URI")
+						.keySerializer(Serializer.STRING).valueSerializer(Serializer.JAVA).createOrOpen();
+				this.uniqueIdentifier2URIs = db.hashMap("uniqueIdentifier2URIs").keySerializer(Serializer.STRING)
+						.valueSerializer(Serializer.JAVA).createOrOpen();
+			}
+		} catch (IOException e) {
+			logger.info("Remote generated does not exist!");
+		}
+
+	}
+
+	private Long readRemoteGenerated() throws IOException {
+		URL generatedURL = new URL(generatedURLString);
+		BufferedReader in = new BufferedReader(new InputStreamReader(generatedURL.openStream()));
+
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = in.readLine()) != null) {
+			sb.append(line);
+		}
+		in.close();
+		return Long.parseLong(sb.toString());
 	}
 
 	public static PreprocessedData getInstance(boolean download) {
@@ -64,6 +104,7 @@ public class PreprocessedData {
 	}
 
 	public void commit() {
+		db.atomicLong(GENERATED, System.currentTimeMillis()).createOrOpen();
 		db.commit();
 	}
 
